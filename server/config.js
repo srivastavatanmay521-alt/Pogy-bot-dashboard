0e83ee887d78310ecd16364a1dc2fee59231ce33
srivastavatanmay521-alt <srivastavatanmay521@gmail.com>
2026-05-10 21:22:28 +0530

    Update config.js

--- server/config.js
+++ server/config.js
@@ -12,17 +12,17 @@ try {
 const defaultSecret =
   process.env.NODE_ENV === "production"
     ? "build-nextauth-secret"
-    : sharedSettings?.Client?.botSecret ||
+    : sharedSettings?.Client?.botSecret || "0wLhmILS9dNpcqhs3_SbD1kI5DSSrKCm"
       sharedSettings?.Client?.Token ||
-      "local-pogyclient-secret";
+      "MTUwMzA0NjY5NjQwMDMyNjY3OA.GzaHEZ.9nRDDeOPNwBdigq0sq0WEEv7tZ7NnqnvRwFXDw";
 
 export function getConfig() {
   return {
-    nextAuthSecret: process.env.NEXTAUTH_SECRET || defaultSecret,
-    discordClientId: process.env.DISCORD_CLIENT_ID || sharedSettings?.Client?.botId || "",
-    discordClientSecret: process.env.DISCORD_CLIENT_SECRET || sharedSettings?.Client?.botSecret || "",
-    discordBotToken: process.env.DISCORD_BOT_TOKEN || sharedSettings?.Client?.Token || "",
-    mongoUri: process.env.MONGODB_URI || sharedSettings?.Database?.Uri || "",
+    nextAuthSecret: process.env.NEXTAUTH_SECRET || defaultsecret,
+    discordClientId: process.env.DISCORD_CLIENT_ID || sharedSettings?.Client?.botId || "1503046696400326678",
+    discordClientSecret: process.env.DISCORD_CLIENT_SECRET || sharedSettings?.Client?.botSecret || "0wLhmILS9dNpcqhs3_SbD1kI5DSSrKCm",
+    discordBotToken: process.env.DISCORD_BOT_TOKEN || sharedSettings?.Client?.Token || "MTUwMzA0NjY5NjQwMDMyNjY3OA.GzaHEZ.9nRDDeOPNwBdigq0sq0WEEv7tZ7NnqnvRwFXDw",
+    mongoUri: process.env.MONGODB_URI || sharedSettings?.Database?.Uri || "mongodb+srv://codex:codex@cdx-in-1.0idu7ol.mongodb.net/?appName=cdx-in-1",
     botApiBaseUrl: process.env.BOT_API_BASE_URL || "http://localhost:3001"
   };
 }
